608ab3b79764d0cf95ff7dd338fb43894d601977
Rafał Maziuk <ziuk.pas@gmail.com>
2021-02-27 23:15:26 +0100

    some changes in exc10

--- src/main/java/petle_i_pliki/zadania/ZP_12.java
+++ src/main/java/petle_i_pliki/zadania/ZP_12.java
@@ -5,17 +5,22 @@ import java.util.Scanner;
 public class ZP_12 {
     public static void main(String[] args) {
 
-        int sellValue, shops, numOfStars=0;
+        int shops;
+        int[] numOfStars;
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("podaj ilosc sklepow: ");
         shops = scanner.nextInt();
 
+        int[] sellValue = new int[shops];
+        numOfStars = new int[sellValue.length];
+
         for (int i = 0; i < shops ; i++) {
             System.out.println("podaj wartosc sprzedazy dla sklepu nr " + (i+1) + ":");
-            sellValue = scanner.nextInt();
-            numOfStars = sellValue / 100;
+            sellValue[i] = scanner.nextInt();
+
+            numOfStars[i] = sellValue[i] / 100;
         }
     }
 }
