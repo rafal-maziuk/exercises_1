242d165bfb9d6559f36ad310d45a52e2db1178d9
Rafał Maziuk <ziuk.pas@gmail.com>
2021-04-12 23:16:37 +0200

    rozdzial zwiazany z operacjami na klasach

---
+++ src/main/java/VI_klasy/zad_prog/zad1/Employee.java
@@ -0,0 +1,16 @@
+package VI_klasy.zad_prog.zad1;
+
+public class Employee {
+
+    String name;
+    int idNumber;
+    String department;
+    String position;
+
+    public Employee(String name,int idNumber, String department, String position) {
+        this.name = name;
+        this.idNumber = idNumber;
+        this.department = department;
+        this.position = position;
+    }
+}
