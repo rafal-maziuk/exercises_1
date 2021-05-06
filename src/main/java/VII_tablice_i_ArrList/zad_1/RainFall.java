5674668235026b8968911af93671bb8d4ef8d9a0
Rafał Maziuk <ziuk.pas@gmail.com>
2021-05-06 23:09:54 +0200

    beginning of chapter 7

---
+++ src/main/java/VII_tablice_i_ArrList/zad_1/RainFall.java
@@ -0,0 +1,20 @@
+package VII_tablice_i_ArrList.zad_1;
+
+public class RainFall {
+
+    double[] tab = new double[12];
+
+
+    public double opadyRok(){
+        return 0;
+    }
+    public double avgMiesiecznyOpad(){
+        return 0;
+    }
+    public String miesiacZNajwOpadow(){
+        return "";
+    }
+    public String miesiacNajmnOpad(){
+        return "";
+    }
+}
---
+++ src/main/java/VII_tablice_i_ArrList/zad_1/main.java
@@ -0,0 +1,7 @@
+package VII_tablice_i_ArrList.zad_1;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
