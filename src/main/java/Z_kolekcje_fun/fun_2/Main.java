25fdddbe85c86be054782abc976611a631ab8934
Rafał Maziuk <ziuk.pas@gmail.com>
2021-05-10 21:06:07 +0200

    HashMap and HashSet

--- src/main/java/Z_kolekcje_fun/fun_2/Main.java
+++ src/main/java/Z_kolekcje_fun/fun_2/Main.java
@@ -1,5 +1,7 @@
 package Z_kolekcje_fun.fun_2;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 
 public class Main {
@@ -16,5 +18,38 @@ public class Main {
             System.out.println(s);
         }
         lista.clear();
+
+        System.out.println("\n======= HashMap ========\n");
+
+        HashMap<Integer, String> mapa = new HashMap<>();
+        mapa.put(1, "Xavi");
+        mapa.put(2, "Puyol");
+        mapa.put(3, "Kluivert");
+        mapa.put(4, "Henry");
+
+        System.out.println(mapa.get(2));
+
+        System.out.println();
+        for (String s : mapa.values()){
+            System.out.println(s);
+        }
+
+        System.out.println(" ===== HashSet =====\n");
+
+        HashSet<String> hSet = new HashSet<>();
+        hSet.add("Rumia");
+        hSet.add("Rumia");
+        hSet.add("Reda");
+        hSet.add("Puck");
+        hSet.add("Wejherowo");
+        hSet.add("Wejherowo");
+        hSet.add("Wejherowo");
+
+        System.out.println();
+        for (String s : hSet){
+            System.out.println(s);
+        }
+        System.out.println(hSet.size());
+
     }
 }
