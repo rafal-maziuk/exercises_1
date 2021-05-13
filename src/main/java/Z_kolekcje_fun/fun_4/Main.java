704c09a1ce16c03c7d8faa306044d71312505c48
Rafał Maziuk <ziuk.pas@gmail.com>
2021-05-13 23:43:48 +0200

    stack stos

--- src/main/java/Z_kolekcje_fun/fun_4/Main.java
+++ src/main/java/Z_kolekcje_fun/fun_4/Main.java
@@ -2,11 +2,12 @@ package Z_kolekcje_fun.fun_4;
 
 import java.util.ArrayDeque;
 import java.util.Queue;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
 
-        Queue<String> kolejka = new ArrayDeque<>();
+        Queue<String> kolejka = new ArrayDeque<>();     // FIFO
 
         kolejka.add("jeden");
         kolejka.add("dwa");
@@ -20,5 +21,20 @@ public class Main {
         System.out.println(kolejka.size());
         System.out.println(kolejka.poll());     //null
         System.out.println(kolejka.size());
+
+
+        System.out.println("\n----stos----");       // LIFO
+        Stack<String> stos = new Stack<>();
+        stos.push("pierwszy");
+        stos.push("drugi");
+        stos.push("trzeci");
+        System.out.println(stos.size());
+        System.out.println(stos.pop());     // zdejmowanie elementu
+        stos.push("ostani");
+        System.out.println(stos.peek());
+        System.out.println(stos.pop());
+        System.out.println(stos.pop());
+        System.out.println(stos.pop());
+        System.out.println(stos.size());
     }
 }
