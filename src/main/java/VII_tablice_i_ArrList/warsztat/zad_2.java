648aad048fd5420521e0c2ea34669662198c9ba5
Rafał Maziuk <ziuk.pas@gmail.com>
2021-05-28 23:43:48 +0200

    zad2

---
+++ src/main/java/VII_tablice_i_ArrList/warsztat/zad_2.java
@@ -0,0 +1,9 @@
+package VII_tablice_i_ArrList.warsztat;
+
+public class zad_2 {
+    public static void main(String[] args) {
+
+        int[] numArray1 = new int[100];
+        int[] numArray2 = new int[100];
+    }
+}
