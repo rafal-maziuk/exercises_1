ecdeb988306034cb1f37c64e14fcaf2ad23e4e64
Rafał Maziuk <ziuk.pas@gmail.com>
2021-05-31 21:14:01 +0200

    kopiowanie tablic

--- src/main/java/VII_tablice_i_ArrList/warsztat/zad_2.java
+++ src/main/java/VII_tablice_i_ArrList/warsztat/zad_2.java
@@ -5,5 +5,18 @@ public class zad_2 {
 
         int[] numArray1 = new int[100];
         int[] numArray2 = new int[100];
+
+        for (int i = 0; i < numArray1.length ; i++) {
+            numArray1[i] = i + 100;
+            int num;
+            num = numArray1[i];
+            System.out.println("START!");
+            System.out.println("arr1 : " + numArray1[i]);
+            System.out.println("aktualne arr2 : " + numArray2[i]);
+            System.out.println("===== kopiowanie nr " + i + " ====");
+            numArray2[i] = num;
+            System.out.println("wynik koncowy arr2: " + numArray2[i]);
+            System.out.println();
+        }
     }
 }
