b90c6e41850d93a39be84e6b8ff72c80aef9d797
Rafał Maziuk <ziuk.pas@gmail.com>
2021-06-14 23:24:56 +0200

    zad 1

--- src/main/java/VII_tablice_i_ArrList/zadania/zad_1/RainFall.java
+++ src/main/java/VII_tablice_i_ArrList/zadania/zad_1/RainFall.java
@@ -1,24 +1,43 @@
 package VII_tablice_i_ArrList.zadania.zad_1;
 
+import java.util.Scanner;
+
 public class RainFall {
 
-    double[] tab = new double[12];
 
-
-    public double opadyRok(double[] tab){
+    public double opadyRok(double[] tab) {
         double sum = 0;
-        for (int i = 0; i < tab.length ; i++) {
+        for (int i = 0; i < tab.length; i++) {
             sum += tab[i];
         }
         return sum;
     }
-    public double avgMiesiecznyOpad(){
-        return 0;
+
+    public double avgMiesiecznyOpad(double sum) {
+        return sum / 12.0;
     }
-    public String miesiacZNajwOpadow(){
-        return "";
+
+    public int miesiacZNajwOpadow(double[] tab) {
+        double maxMonth = tab[0];
+        int monthNr = 1;
+        for (int i = 1; i < tab.length; i++) {
+            if (tab[i] > maxMonth) {
+                maxMonth = tab[i];
+                monthNr = i+1;
+            }
+        }
+        return monthNr;
     }
-    public String miesiacNajmnOpad(){
-        return "";
+
+    public int miesiacNajmnOpad(double[] tab) {
+        double minMonth = tab[0];
+        int monthNr = 1;
+        for (int i = 1; i < tab.length; i++) {
+            if (tab[i] < minMonth) {
+                minMonth = tab[i];
+                monthNr = i+1;
+            }
+        }
+        return monthNr;
     }
 }
--- src/main/java/VII_tablice_i_ArrList/zadania/zad_1/main.java
+++ src/main/java/VII_tablice_i_ArrList/zadania/zad_1/main.java
@@ -1,7 +1,29 @@
 package VII_tablice_i_ArrList.zadania.zad_1;
 
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
+        double[] tab = new double[12];
+        Scanner scanner = new Scanner(System.in);
 
+        for (int i = 0; i < tab.length; i++) {
+            int opad = 0;
+            System.out.println("podaj opad w mm dla miesiąca nr " + (i + 1) + " : ");
+            opad = scanner.nextInt();
+            if (opad<0){
+                System.out.println("błędne dane wejściowe!");
+                System.exit(-1);
+            }
+            tab[i] = opad;
+        }
+
+        RainFall rainFall = new RainFall();
+
+        double opadytotal = rainFall.opadyRok(tab);
+        System.out.println("laczne opady: " + opadytotal);
+        System.out.println("srednie: " + rainFall.avgMiesiecznyOpad(opadytotal));
+        System.out.println("miesiac max: " + rainFall.miesiacZNajwOpadow(tab));
+        System.out.println("miesiac min: " + rainFall.miesiacNajmnOpad(tab));
     }
 }
