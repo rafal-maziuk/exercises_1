9bf5878b20cc91eadbde455d87f977a81a6e495e
Rafał Maziuk <ziuk.pas@gmail.com>
2021-06-24 22:48:24 +0200

    poczatek zad 2

--- src/main/java/VII_tablice_i_ArrList/zadania/zad_2/Payroll.java
+++ src/main/java/VII_tablice_i_ArrList/zadania/zad_2/Payroll.java
@@ -3,9 +3,10 @@ package VII_tablice_i_ArrList.zadania.zad_2;
 public class Payroll {
 
     int[] employeeId = {5658845, 4520125, 7895122, 8777541, 8451277, 1302850, 7580489};
-    int[] hours;
-    double[] payRate;
-    double[] wages;
+    int[] hours = new int[7];
+    double[] payRate = new double[7];
+    double[] wages = new double[7];
+
 
 
 }
