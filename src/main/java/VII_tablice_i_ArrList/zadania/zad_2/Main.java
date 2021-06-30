4bca9ed036369482433f3fafdce824b41f562c40
Rafał Maziuk <ziuk.pas@gmail.com>
2021-06-30 23:32:22 +0200

    zad 2 in progress

---
+++ src/main/java/VII_tablice_i_ArrList/zadania/zad_2/Main.java
@@ -0,0 +1,24 @@
+package VII_tablice_i_ArrList.zadania.zad_2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Payroll payroll = new Payroll();
+
+
+        for (int i = 0; i < payroll.employeeId.length; i++) {
+            double brutto = 0;
+            System.out.println("podaj liczbe przepracowanych godzin pracownika nr " + (payroll.employeeId[i]) + " :");
+            payroll.hours[i] = scanner.nextInt();
+            System.out.println("podaj stawkę godzinową pracownika nr " + (payroll.employeeId[i]) + " :");
+            payroll.payRate[i] = scanner.nextDouble();
+
+            brutto = payroll.hours[i] * payroll.payRate[i];
+            payroll.wages[i] = brutto;
+        }
+
+    }
+}
--- src/main/java/VII_tablice_i_ArrList/zadania/zad_2/Payroll.java
+++ src/main/java/VII_tablice_i_ArrList/zadania/zad_2/Payroll.java
@@ -4,9 +4,52 @@ public class Payroll {
 
     int[] employeeId = {5658845, 4520125, 7895122, 8777541, 8451277, 1302850, 7580489};
     int[] hours = new int[7];
+//    int[] hours = {100, 120, 140, 160, 180, 200, 220};
     double[] payRate = new double[7];
+//    double[] payRate = {20, 22, 18, 17, 14, 24, 21};
     double[] wages = new double[7];
 
+    public Payroll() {
+    }
+
+    public double wynagrodzenie(int id){
 
 
+
+        return 0;
+    }
+
+
+
+    public int[] getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(final int[] employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int[] getHours() {
+        return hours;
+    }
+
+    public void setHours(final int[] hours) {
+        this.hours = hours;
+    }
+
+    public double[] getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(final double[] payRate) {
+        this.payRate = payRate;
+    }
+
+    public double[] getWages() {
+        return wages;
+    }
+
+    public void setWages(final double[] wages) {
+        this.wages = wages;
+    }
 }
