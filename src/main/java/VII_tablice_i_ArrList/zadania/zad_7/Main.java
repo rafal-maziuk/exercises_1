3f270951c1b64ca4144361f927d0c238dceb3d9a
Rafał Maziuk <ziuk.pas@gmail.com>
2021-08-01 22:44:34 +0200

    zad7 finish

--- src/main/java/VII_tablice_i_ArrList/zadania/zad_7/Main.java
+++ src/main/java/VII_tablice_i_ArrList/zadania/zad_7/Main.java
@@ -1,13 +1,17 @@
 package VII_tablice_i_ArrList.zadania.zad_7;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        PrintWriter outputFile = new PrintWriter("odpowiedzi.txt");
+        String filename = "odpowiedzi.txt";
+        PrintWriter outputFile = new PrintWriter(filename);
         outputFile.println("tak, oczywiście!");
         outputFile.println("bez wątpienia.");
         outputFile.println("możesz na to liczyć");
@@ -24,8 +28,32 @@ public class Main {
 
         ArrayList<String> arrayList = new ArrayList<>();
 
+        File file = new File(filename);
+        Scanner inputFile = new Scanner(file);
 
+        while (inputFile.hasNext()){
+            String ans = inputFile.nextLine();
+            arrayList.add(ans);
+        }
+        inputFile.close();
 
-        
+        String userDecison, inputAns;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            Random random = new Random();
+            int randomNr = random.nextInt(arrayList.size());
+            System.out.println("masz jakieś pytanie?");
+            inputAns = scanner.nextLine();
+
+            System.out.println("hmm, niech pomyślę...");
+            System.out.println(arrayList.get(randomNr));
+            System.out.println();
+            System.out.println("czy chesz zapytać o coś jeszcze?");
+            System.out.println("wpisz 't' lub 'T' jeśli chcesz kontynuować: ");
+            userDecison = scanner.nextLine().substring(0,1);
+
+        }
+        while (userDecison.equalsIgnoreCase("t"));
     }
 }
