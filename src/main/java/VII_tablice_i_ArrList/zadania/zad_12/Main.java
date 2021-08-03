6cf57063176ac73e6e3b265a8e5fb418917f6864
Rafał Maziuk <ziuk.pas@gmail.com>
2021-08-03 12:18:27 +0200

    start of zad12

---
+++ src/main/java/VII_tablice_i_ArrList/zadania/zad_12/Main.java
@@ -0,0 +1,18 @@
+package VII_tablice_i_ArrList.zadania.zad_12;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        String fileName = "1994_Weekly_Gas_Averages.txt";
+        File file = new File(fileName);
+        Scanner scanner = new Scanner(file);
+
+
+    }
+}
