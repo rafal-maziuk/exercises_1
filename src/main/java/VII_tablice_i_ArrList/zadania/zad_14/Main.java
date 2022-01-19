fbf4be6052644195c303158d0d87487813c7e38d
Rafał Maziuk <ziuk.pas@gmail.com>
2022-01-19 23:53:25 +0100

    exc14 going on

--- src/main/java/VII_tablice_i_ArrList/zadania/zad_14/Main.java
+++ src/main/java/VII_tablice_i_ArrList/zadania/zad_14/Main.java
@@ -1,10 +1,13 @@
 package VII_tablice_i_ArrList.zadania.zad_14;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
         String girlName, boyName;
-
+        ArrayList<String> girlNameList = new ArrayList<>();
+        ArrayList<String> boyNameList = new ArrayList<>();
 
 
     }
