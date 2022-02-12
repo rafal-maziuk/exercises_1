3bcea549fb0b7f7d31ef4d0e5f482855db19136f
Rafał Maziuk <ziuk.pas@gmail.com>
2022-02-12 21:32:01 +0100

    exc14

--- src/main/java/VII_tablice_i_ArrList/zadania/zad_14/Main.java
+++ src/main/java/VII_tablice_i_ArrList/zadania/zad_14/Main.java
@@ -1,13 +1,36 @@
 package VII_tablice_i_ArrList.zadania.zad_14;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        String girlName, boyName;
+        File fileGirlName = new File("C:\\_IT\\cwiczenia\\exc_1\\src\\main\\java\\VII_tablice_i_ArrList\\zadania\\zad_14\\GirlNames.txt"),
+                fileBoyName = new File("BoyNames.txt");
+        Scanner input1 = new Scanner(fileGirlName);
+        Scanner input2 = new Scanner(fileBoyName);
         ArrayList<String> girlNameList = new ArrayList<>();
         ArrayList<String> boyNameList = new ArrayList<>();
 
+        method(input1, girlNameList);
+        method(input2, boyNameList);
+
+        System.out.println(girlNameList);
+        System.out.println(boyNameList);
+    }
+
+    public static void method(@NotNull Scanner scan, List list){
+        while (scan.hasNext()){
+            String name = scan.nextLine();
+            list.add(name);
+        }
+        scan.close();
+        System.out.println();
     }
 }
