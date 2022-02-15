39a7dd054e1cf7843c02d28580db3a6b12fc8fe1
Rafał Maziuk <ziuk.pas@gmail.com>
2022-02-15 23:21:08 +0100

    exc17 start

---
+++ src/main/java/VII_tablice_i_ArrList/zadania/zad_17/Main.java
@@ -0,0 +1,34 @@
+package VII_tablice_i_ArrList.zadania.zad_17;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[][] table;
+
+    }
+
+    public static int getTotal(int[][] tab){
+        return 0;
+    }
+
+    public static int getAverage(int[][] tab){
+        return 0;
+    }
+
+    public static int getRowTotal(int[][] tab, int nr){
+        return 0;
+    }
+
+    public static int getColumnTotal(int[][] tab, int nr){
+        return 0;
+    }
+
+    public static int getHighestInRow(int[][] tab, int nr){
+        return 0;
+    }
+
+    public static int getLowestInRow(int[][] tab, int nr){
+        return 0;
+    }
+}
