cfff4c9f41790da9a909f4e4f85c12d2117ce35c
Rafał Maziuk <ziuk.pas@gmail.com>
2022-02-22 22:30:43 +0100

    little changes

--- src/main/java/VII_tablice_i_ArrList/zadania/zad_17/Main.java
+++ src/main/java/VII_tablice_i_ArrList/zadania/zad_17/Main.java
@@ -4,9 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[][] table = { {1,2,3} , {4,5,6} , {7,8,9}};
+        int[][] table = { {1,2,3} ,
+                        {4,5,6} ,
+                        {7,8,9}};
 
         System.out.println("suma w tabeli wynosi: " + getTotal(table));
+        System.out.println("średnia: " + getAverage(table));
+        System.out.println("wartość wiersza: " + getRowTotal(table, 0));
+        System.out.println("wartość kolumny: " + getColumnTotal(table, 1));
 
     }
 
@@ -20,16 +25,50 @@ public class Main {
         return sum;
     }
 
-    public static int getAverage(int[][] tab){
-        return 0;
+    public static double getAverage(int[][] tab){
+        int sum = 0;
+        int avg = 0;
+        for (int i = 0; i < tab.length; i++) {
+            for (int j = 0; j < tab[i].length; j++) {
+                sum += tab[i][j];
+                avg += 1;
+            }
+        }
+        return (double)sum/avg;
     }
 
-    public static int getRowTotal(int[][] tab, int nr){
-        return 0;
+    public static int getRowTotal(int[][] tab, int nrRow){
+        int rowTotal = 0;
+        try {
+            if (nrRow < 0 || nrRow > tab.length) {
+                return -1;
+            } else {
+                for (int i = 0; i < tab.length; i++) {
+                    rowTotal += tab[nrRow][i];
+                }
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("index poza rozmiarem tablicy !");
+            System.exit(-1);
+        }
+        return rowTotal;
     }
 
-    public static int getColumnTotal(int[][] tab, int nr){
-        return 0;
+    public static int getColumnTotal(int[][] tab, int nrCol){
+        int colTotal = 0;
+        try {
+            if (nrCol < 0 || nrCol > tab.length) {
+                return -1;
+            } else {
+                for (int i = 0; i < tab.length; i++) {
+                    colTotal += tab[i][nrCol];
+                }
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("index poza rozmiarem tablicy !");
+            System.exit(-1);
+        }
+        return colTotal;
     }
 
     public static int getHighestInRow(int[][] tab, int nr){
